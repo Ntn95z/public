39d0eead7630839152692288792918c659d8f1e5
Ntn95z <toinoudanjoux333@gmail.com>
2026-06-15 07:54:49 -0700

    Update discordbot.py

--- Youtube/discordbot.py
+++ Youtube/discordbot.py
@@ -1,10 +1,9 @@
-# Imports
+# Imports discord
 import discord
 from discord import app_commands
 from discord.ext import commands
 import requests
-from config import token, apikey
-
+from config import 
 # Bot Configuration
 intents = discord.Intents.default() 
 intents.message_content = True
