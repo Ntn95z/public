1484f8ba329cb9c6c871eb98fdae2439907111d9
Ntn95z <toinoudanjoux333@gmail.com>
2026-06-15 07:57:24 -0700

    Update config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -1,2 +1,2 @@
-token = 'REPLACE WITH DISCORD TOKEN'
-apikey = 'REPLACE WITH API KEY'
+token = MTUxNTEyMDI4NDQ2ODkwODA3Mw.GnEhFj.nnrojIkvH7-wgMu1vmhwUTU0Yi1Zt2Vh1TkNdU
+apikey = 020c76891e0f4f2fb3d5ae8794f2e7ff
